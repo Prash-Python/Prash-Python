e07b583a2687fb340163d5f5aaa8751aab0133bb
Prash-Python <prash.javadev1@gmail.com>
2025-12-05 14:53:47 +0530

    Add files via upload

--- Python_Hcl/session3_loops_exercises3.py
+++ Python_Hcl/session3_loops_exercises3.py
@@ -24,3 +24,14 @@ for i in range(5, 0, -1):
     for j in range(i):
         print('#', end='')
     print()  # Move to the next line after each row
+
+# Count the number of vowels in a given string.
+def countVowels(input_string):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for char in input_string:
+        if char in vowels:
+            count += 1
+    print(f"Number of vowels in the given string: {count}")
+    print(f"Number of consonants in the given string: {len(input_string) - count}")
+countVowels(input("Enter a string: "))
