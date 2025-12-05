c3a2ddbe9b617cc4a46ec87d31eb13473a0df771
Prash-Python <prash.javadev1@gmail.com>
2025-12-05 14:40:41 +0530

    Add files via upload

--- Python_Hcl/session3_loops_exercises3.py
+++ Python_Hcl/session3_loops_exercises3.py
@@ -19,3 +19,8 @@ for i in range(5):
     for j in range(i+1):
         print('*', end='')
     print()  # Move to the next line after each row
+
+for i in range(5, 0, -1):
+    for j in range(i):
+        print('#', end='')
+    print()  # Move to the next line after each row
