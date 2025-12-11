7d7548330623432c5f4fa11df5740534c1130672
Prash-Python <prash.javadev1@gmail.com>
2025-12-11 08:28:11 +0530

    Add files via upload

--- Python_Hcl/session11_regular_exp_exercises.py
+++ Python_Hcl/session11_regular_exp_exercises.py
@@ -1,70 +1,3 @@
-import re
-import math
-from re import split
-
-given_string = "Computer Science 12345 python 78901 java 98765 !@$87642"
-
-# search.
-m = re.search("Science", given_string)
-print(m.start())
-print(m.end())
-print(type(m))
-print(m)
-
-# Find all.
-p = re.compile(r'\d+')
-digits = re.findall(p, given_string)
-print(digits)
-
-# Compile.
-p = re.compile(r'[a-e]')
-print(p.findall(given_string))
-
-p = re.compile('[aeiouAEIOU]')
-print(p.findall(given_string))
-print(len(p.findall(given_string)))
-
-# Single character.
-p = re.compile(r'\w')
-print(p.findall(given_string))
-
-# Single word.
-p = re.compile(r'\w+')
-print(p.findall(given_string))
-
-# Single non-word character.
-p = re.compile(r'\W')
-print(p.findall(given_string))
-
-# Can be zero or more b.
-given_string = "abaababbbaba"
-p = re.compile('ab*')
-print(p.findall(given_string))
-
-# Can be one or more b.
-p = re.compile('ab+')
-print(p.findall(given_string))
-
-# split function.
-print(split(r'\w+', 'Words words Words'))
-print(split(r'\W', 'Words words Words'))
-print(split(r'\W+', 'Words words Words'))
-print(split(r'\d+', 'Words 12 18 words 9 5 Words'))
-
-# re.sub() function.
-print(re.sub('ab|AB','**', 'abcdeABpr', re.IGNORECASE))
-
-# Starts with -
-p = r'^Hello'
-t = "Hello Python World"
-match = re.search(p, t)
-print(match)
-# Ends with $
-p = r'World$'
-match = re.search(p,t)
-print(match)
-
-
 # "Contact us at support@example.com or sales@shop.com"
 def extract_email_with_re(given_text):
     pattern = re.compile(r'[a-z]+@[a-z]+.[a-z]')
@@ -140,7 +73,3 @@ Define a custom exception InvalidAgeError
 class InvalidAgeError(Exception):
     print("This is the error handling")
     pass
-
-
-
-
